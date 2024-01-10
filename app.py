ab4eca2d92dd1523dd17de26a71e66fc631d25e2
Thaw De Zin <thawdezin@gmail.com>
2024-01-11 04:33:48 +0630

    my website

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! This is a simple Flask app.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
